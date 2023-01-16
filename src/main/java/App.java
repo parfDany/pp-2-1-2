067d51cdd14623582852f57481b64932a451c24f
parfDany <dany991@yandex.ru>
2023-01-16 17:42:11 +0300

    2.1.2 spring-hello-cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld beanOne = (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Два объекта равны?" + (beanOne == beanTwo));
+
+        Cat beenThree = (Cat)applicationContext.getBean("cat");
+
+        Cat beenFour = (Cat)applicationContext.getBean("cat");
+
+        System.out.println("Два объекта равны?" + (beenThree == beenFour));
     }
+
+
 }
